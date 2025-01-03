dd538b2441cc91a195bbe85a4903ffe3515cd4ba
tanishkaa08 <162544044+tanishkaa08@users.noreply.github.com>
2025-01-03 21:53:20 +0530

    appwrite services

---
+++ visionboard/src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  console.log(import.meta.env.VITE_APPWRIE_URL)
+
+  return (
+    <>
+      <h1>Vision board
+      </h1>
+    </>
+  )
+}
+
+export default App
